8276dc320a7f7cc399dc35e96f68ec9342194256
Steve Tipton <TIPTONSJ@innovasi.com>
2017-09-13 12:56:01 -0400

    Handling character literals with escaping

--- DisplayingText/DisplayingText/DisplayingText.py
+++ DisplayingText/DisplayingText/DisplayingText.py
@@ -10,3 +10,6 @@ print("here is a string " + " here is more")
 
 print('here is a double quote "' + " here is a single quote '")
 print("or you can just do this \" does that work")
+
+# you have to escape the literal \n so it is ignored
+print("what if I want to have a slash like \\news in my string?")
