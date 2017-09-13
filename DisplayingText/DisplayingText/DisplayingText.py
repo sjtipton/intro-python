d95002d2646d57ed82b0606d3d47ed7135790183
Steve Tipton <TIPTONSJ@innovasi.com>
2017-09-13 12:49:08 -0400

    Playing with multiple lines and quotes

--- DisplayingText/DisplayingText/DisplayingText.py
+++ DisplayingText/DisplayingText/DisplayingText.py
@@ -1,3 +1,12 @@
 print('The capybara is the worlds largest rodent')
 print('The capybara likes to live in groups')
 print("The capybara can swim")
+
+print("The capybara lives in \nSouth America")
+print("""This is the strangest
+way to print over multiple lines I know""")
+
+print("here is a string " + " here is more")
+
+print('here is a double quote "' + " here is a single quote '")
+print("or you can just do this \" does that work")
